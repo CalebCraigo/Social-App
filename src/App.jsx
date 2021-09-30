5d6c16a270b2069b7412c2628382c8e02d163b1e
Caleb Craigo <CalebDOS1@yahoo.com>
2021-09-30 19:59:55 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login">
+
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+
+function Home() {
+
+    render()
+        return(
+            <h1>Hello World!</h1>
+        )
+        
+    
+}
+
+export default Home;
