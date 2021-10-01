82b0e3c37216301fa8b527915e458284f13fdce9
Caleb Craigo <CalebDOS1@yahoo.com>
2021-09-30 22:46:38 -0400

    Added mock data to feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,56 @@
 import React from "react";
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+const feedData = [
+    {
+        name: "John Smith",
+        timePosted: 500,
+        text: "This is a test Post"
+    },
+    {
+        name: "John Smith",
+        timePosted: 500,
+        text: "This is a test Post"
+    },
+    {
+        name: "John Smith",
+        timePosted: 500,
+        text: "This is a test Post"
+    },
+    {
+        name: "John Smith",
+        timePosted: 500,
+        text: "This is a test Post"
+    },
+    {
+        name: "John Smith",
+        timePosted: 500,
+        text: "This is a test Post"
+    }
+]
 
 function Feed() {
 
+    const post = feedData.map((data)=> 
+        <React.Fragment>
+            <p>{data.name}</p>
+            <p>{data.text}</p>
+        </React.Fragment>
+    )
+
     return(
-        <p>Friends Posts go here</p>
+        <React.Fragment>
+            <Stack
+                direction="column"
+                justifyContent="flex-start"
+                alignItems="left"
+                spacing={1}
+            >
+                {post}
+            </Stack>
+        </React.Fragment>
     )
 }
 
